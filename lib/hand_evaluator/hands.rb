ab78e1b2e24082d4e3abcb0ec6992a97eeba6b25
Mark A. Chandler, II <mark.allen.chandler@gmail.com>
2015-08-12 19:18:50 -0400

    Add hands list global constant

--- lib/hand_evaluator/hands.rb
+++ lib/hand_evaluator/hands.rb
@@ -8,3 +8,16 @@ class ThreeOfAKind < Hand; end
 class TwoPair < Hand; end
 class OnePair < Hand; end
 class Flop < Hand; end
+
+$HANDS_LIST = [
+  Flop,
+  OnePair,
+  TwoPair,
+  ThreeOfAKind,
+  Straight,
+  Flush,
+  FullHouse,
+  FourOfAKind,
+  StraightFlush,
+  RoyalFlush
+]
